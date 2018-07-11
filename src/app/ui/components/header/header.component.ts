2921a86eff01ad2cc12f9d58f521cde37798dbeb
Alexey <asdalexey@yandex.ru>
2018-07-11 21:20:15 +0300

    #5 create layout

--- src/app/ui/components/header/header.component.ts
+++ src/app/ui/components/header/header.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  logo = 'assets/images/logo.svg';
+  title = 'Store';
+  links = [
+    { label: 'Products', url: '/products' },
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
-
 }

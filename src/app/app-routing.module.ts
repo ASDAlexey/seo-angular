2389ad23cc78433b225bbe9b5cb0dbf4cbb62a84
Alexey <asdalexey@yandex.ru>
2018-07-11 21:31:40 +0300

    #6 make basic product component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { LayoutComponent } from './ui/components/layout/layout.component';
 
 const routes: Routes = [
-  { path: '', component: LayoutComponent, children: [] }
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: '/products' },
+      { path: 'products', loadChildren: './product/product.module#ProductModule' },
+    ]
+  }
 ];
 
 @NgModule({
---
+++ src/app/product/models/product.ts
@@ -0,0 +1,7 @@
+export class Product {
+  id: string;
+  name: string;
+  describtion: string;
+  image: string;
+  price: number;
+}

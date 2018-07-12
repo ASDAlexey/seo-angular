b024feeb5d57ff865ebed45ac71ad4da4d83c98f
Alexey <asdalexey@yandex.ru>
2018-07-12 19:46:38 +0300

    #6 add product list anf product details data

--- src/app/product/containers/product-detail/product-detail.component.ts
+++ src/app/product/containers/product-detail/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../../models/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent implements OnInit {
+  product: Product = new Product();
 
-  constructor() { }
+  constructor(private router: ActivatedRoute) {}
 
   ngOnInit() {
+    this.product.id = this.router.snapshot.paramMap.get('id');
   }
-
 }
--- src/app/product/containers/product-list/product-list.component.ts
+++ src/app/product/containers/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-product-list',
@@ -6,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
+  products: Product[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
